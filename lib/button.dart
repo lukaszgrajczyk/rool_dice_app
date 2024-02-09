281af9ccd657f5677e6d378ea30bf1092c0a17f7
Lukasz Grajczyk <lukaszgrajczyk1988@gmail.com>
2024-02-09 15:45:35 +0100

    Set background color in app

---
+++ lib/button.dart
@@ -0,0 +1,22 @@
+// import 'package:flutter/material.dart';
+
+// class Button extends StatelessWidget {
+//   const Button({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return OutlinedButton(
+//       style: OutlinedButton.styleFrom(
+//           fixedSize: const Size(210, 50),
+//           foregroundColor: Colors.yellow,
+//           backgroundColor: Colors.black),
+//       onPressed: () {},
+//       child: const Text(
+//         'Roll dice',
+//         style: TextStyle(fontSize: 35),
+//       ),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Scaffold(),
+      title: 'Flutter First App',
+      theme: ThemeData(useMaterial3: true),
+      home: const HomePage(),
     );
   }
 }
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.green, Color.fromARGB(255, 161, 181, 36)],
+          begin: Alignment.bottomCenter,
+          end: Alignment.centerLeft,
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+ 
+   
+  //     child: Center(
+  //       child: Column(
+  //         mainAxisSize: MainAxisSize.min,
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: [
+  //           Image.asset(width: 250, 'assets/images/dice-1.png'),
+  //           const SizedBox(height: 20),
+  //           const Button(), //button class customization
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
